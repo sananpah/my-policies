795ceb0f9293fb12486c52156770f13729477896
sananpah <127280855+sananpah@users.noreply.github.com>
2026-03-17 22:25:19 +0800

    Update data_health.js

--- data_health.js
+++ data_health.js
@@ -37,9 +37,9 @@ export const healthData = [
         benefits: [
             "Emergency and OutPatient Care: $2000 / year",
             "Cancer Care Booster: 21x Medishield Limit",
-            "Non Cancer Drug List: $200,000 / year"
+            "Non Cancer Drug List: $200,000 / year",
             "Co-insurance cap: $50,000 /year",
-            "Public Hospital 5% co-payment (cap at $3,000 / year)"
+            "Public Hospital 5% co-payment (cap at $3,000 / year)",
             "Private Hospital Deductible: $2,000"
         ],
         nominee: "N/A",
