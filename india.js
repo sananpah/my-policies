d367bf8ee6acacd0278a0d5a16782bc439e101ed
sananpah <127280855+sananpah@users.noreply.github.com>
2026-04-04 23:04:17 +0800

    Update india.js

--- india.js
+++ india.js
@@ -36,3 +36,8 @@ const TODAY = new Date();
             const n = parseFloat(clean);
             return isNaN(n) ? 0 : n;
         }
+
+      export const monthMap = {
+          "Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3, "May": 4, "Jun": 5,
+          "Jul": 6, "Aug": 7, "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11
+      };
