b601ed0f682350f26107438437dfff5a995c670e
sananpah <127280855+sananpah@users.noreply.github.com>
2026-04-05 12:59:03 +0800

    Update data.js

--- data.js
+++ data.js
@@ -6,8 +6,6 @@ export const POLICY_DATA = {
             id: "54751949",
             uin: "128N171V01",
             clientId: "60938507",
-            premiumEnds: "17 Nov 2034",
-            maturity: "17 Nov 2055", 
             color: "#962524",
             maturityAmt: "₹ 2,626,948",
             payoutSchedule: { 11: 133974, 12: 133974, 13: 133974, 14: 133974, 15: 133974, 16: 160769, 17: 160769, 18: 160769, 19: 160769, 20: 160769, 21: 187564, 22: 187564, 23: 187564, 24: 187564, 25: 187564, 26: 214359, 27: 214359, 28: 214359, 29: 214359, 30: 214359 }
@@ -17,8 +15,6 @@ export const POLICY_DATA = {
             id: "54679062",
             uin: "128N161V04",
             clientId: "60938507",
-            premiumEnds: "30 May 2030",
-            maturity: "30 May 2077", 
             color: "#962524",
             annualPayout: 89830,
             maturityAmt: "Special Surrender Value",
@@ -29,8 +25,6 @@ export const POLICY_DATA = {
             id: "54040166",
             uin: "128N154V02",
             clientId: "60938507",
-            premiumEnds: "10 Jan 2028",
-            maturity: "10 Jan 2050", 
             color: "#962524",
             maturityAmt: "₹ 1,750,854",
             payoutSchedule: { 8: 100000, 9: 100000, 10: 100000, 11: 100000, 12: 100000, 13: 100000, 14: 100000, 15: 100000, 16: 100000, 17: 100000, 18: 100000, 19: 100000, 20: 100000, 21: 100000, 22: 100000, 23: 100000, 24: 100000, 25: 100000, 26: 100000, 27: 100000, 28: 100000 }
@@ -40,8 +34,6 @@ export const POLICY_DATA = {
             id: "53044042",
             uin: "128N053V02",
             clientId: "60938507",
-            premiumEnds: "08 Sep 2026",
-            maturity: "08 Sep 2032", 
             color: "#962524",
             maturityAmt: "Loyalty: ₹ 37,170 + Mat: ₹ 18,591", 
             payoutSchedule: { 11: 18591, 12: 18591, 13: 18591, 14: 18591, 15: 49576 } 
@@ -51,8 +43,6 @@ export const POLICY_DATA = {
             id: "79079094",
             uin: "107N105V02",
             clientId: "61491746",
-            premiumEnds: "13 Mar 2031",
-            maturity: "13 Mar 2040", 
             color: "#e63946",
             maturityAmt: "₹ 3,23,695 + Bonus"
         },
@@ -61,8 +51,6 @@ export const POLICY_DATA = {
             id: "79425536",
             uin: "107N124V01",
             clientId: "61491746",
-            premiumEnds: "14 Aug 2034",
-            maturity: "14 Aug 2089", 
             color: "#e63946",
             bonusAmount: 22050,
             bonusStartYear: 2,
@@ -72,8 +60,6 @@ export const POLICY_DATA = {
             logo: "image_86c684.png",
             id: "503-9562771",
             uin: "130A012V02",
-            premiumEnds: "28 Sep 2029",
-            maturity: "28 Sep 2038", 
             color: "#005a9c",
             maturityAmt: "Unit Value",
             currentUnitValue: "159421"
@@ -82,8 +68,6 @@ export const POLICY_DATA = {
             logo: "image_7a80d9.png",
             id: "608480927",
             uin: "104L082V02",
-            premiumEnds: "04 Feb 2026",
-            maturity: "04 Feb 2032", 
             color: "#ae125d",
             maturityAmt: "Unit Value",
             currentUnitValue: "277547"
@@ -92,8 +76,6 @@ export const POLICY_DATA = {
             logo: "image_349b41.png",
             id: "425335400",
             uin: "116L153V02",
-            premiumEnds: "06 Oct 2025",
-            maturity: "05 Nov 2030", 
             color: "#005a9c",
             maturityAmt: "Unit Value",
             currentUnitValue: "454673"
@@ -102,8 +84,6 @@ export const POLICY_DATA = {
             logo: "image_349b41.png",
             id: "378816898",
             uin: "116L181V01",
-            premiumEnds: "28 Oct 2024",
-            maturity: "27 Nov 2029", 
             color: "#005a9c",
             maturityAmt: "Unit Value",
             currentUnitValue: "208508"
@@ -113,8 +93,6 @@ export const POLICY_DATA = {
             id: "K7639543",
             uin: "N/A",
             clientId: "N/A",
-            premiumEnds: "29 Nov 2032",
-            maturity: "29 Nov 2052", 
             color: "#9b2226",
             maturityAmt: "Unit Value",
             currentUnitValue: "442990"
@@ -124,8 +102,6 @@ export const POLICY_DATA = {
             id: "3N307267702",
             uin: "N/A",
             clientId: "N/A",
-            premiumEnds: "23 Apr 2029",
-            maturity: "23 Apr 2040", 
             color: "#00B4EE",
             maturityAmt: "Unit Value",
             currentUnitValue: "556047"
@@ -136,7 +112,6 @@ export const POLICY_DATA = {
             logo: "image_7aee9a.png", 
             id: "34874419", 
             totalPremiumPaid: 69176.18, // Added to reflect premium holidays
-            maturity: "31 Jan 2029", 
             color: "#ED1B2E",             
             currentUnitValue: "24671",            
             withdrawals: [9195], 
@@ -147,7 +122,6 @@ export const POLICY_DATA = {
             logo: "image_7aee9a.png", 
             id: "33401114", 
             totalPremiumPaid: 72835.86, // Added to reflect premium holidays
-            maturity: "04 Jul 2086", 
             color: "#ED1B2E",             
             currentUnitValue: "9071",            
             withdrawals: [27481.38, 12072.37], 
@@ -157,7 +131,6 @@ export const POLICY_DATA = {
         { 
             logo: "image_4b9b3f.png",
             id: "U126327913",
-            maturity: "30 Dec 2077", 
             color: "#d31145",
             surrenderCharges: { 1: 100, 2: 100, 3: 80, 4: 70, 5: 60, 6: 50, 7: 45, 8: 35, 9: 20, 10: 5, 11: 0 },
             currentUnitValue: "27589",
@@ -166,7 +139,6 @@ export const POLICY_DATA = {
         { 
             logo: "image_d1e382.png",
             id: "2451556041",
-            maturity: "26 Dec 2076", 
             color: "#00a758",            
             surrenderCharges: { 1: 100, 2: 100, 3: 79, 4: 60, 5: 50, 6: 47, 7: 44, 8: 21, 9: 16, 10: 8, 11: 0 },
             currentUnitValue: "19460",
@@ -175,7 +147,6 @@ export const POLICY_DATA = {
         { 
             logo: "image_8bcf5a.png",
             id: "104-1315514",
-            maturity: "21 Feb 2076", 
             color: "#db0011",            
             surrenderCharges: { 1: 100, 2: 52, 3: 49, 4: 45, 5: 40, 6: 32, 7: 26, 8: 20, 9: 14, 10: 8, 11: 0 },
             currentUnitValue: "14742",
@@ -184,7 +155,6 @@ export const POLICY_DATA = {
         { 
             logo: "image_89093e.png",
             id: "P4172398", 
-            maturity: "02 Jul 2776", 
             color: "#E60000",            
             surrenderCharges: { 1: 100, 2: 100, 3: 80, 4: 60, 5: 50, 6: 45, 7: 40, 8: 20, 9: 15, 10: 10, 11: 0 },
             currentUnitValue: "16205",
