0727129fa85ae68ac06b0edc4dc38817487bf79a
sananpah <127280855+sananpah@users.noreply.github.com>
2026-04-05 23:16:47 +0800

    Update data.js

--- data.js
+++ data.js
@@ -6,8 +6,7 @@ export const POLICY_DATA = {
             id: "54751949",
             uin: "128N171V01",
             clientId: "60938507",
-            color: "#962524",
-            maturityAmt: "₹ 2,626,948",
+            color: "#962524",           
             payoutSchedule: { 11: 133974, 12: 133974, 13: 133974, 14: 133974, 15: 133974, 16: 160769, 17: 160769, 18: 160769, 19: 160769, 20: 160769, 21: 187564, 22: 187564, 23: 187564, 24: 187564, 25: 187564, 26: 214359, 27: 214359, 28: 214359, 29: 214359, 30: 214359 }
         },
         { 
@@ -16,8 +15,7 @@ export const POLICY_DATA = {
             uin: "128N161V04",
             clientId: "60938507",
             color: "#962524",
-            annualPayout: 89830,
-            maturityAmt: "Special Surrender Value",
+            annualPayout: 89830,            
             defermentYear: 2031
         },
         { 
@@ -26,7 +24,6 @@ export const POLICY_DATA = {
             uin: "128N154V02",
             clientId: "60938507",
             color: "#962524",
-            maturityAmt: "₹ 1,750,854",
             payoutSchedule: { 8: 100000, 9: 100000, 10: 100000, 11: 100000, 12: 100000, 13: 100000, 14: 100000, 15: 100000, 16: 100000, 17: 100000, 18: 100000, 19: 100000, 20: 100000, 21: 100000, 22: 100000, 23: 100000, 24: 100000, 25: 100000, 26: 100000, 27: 100000, 28: 100000 }
         },
         { 
@@ -34,8 +31,7 @@ export const POLICY_DATA = {
             id: "53044042",
             uin: "128N053V02",
             clientId: "60938507",
-            color: "#962524",
-            maturityAmt: "Loyalty: ₹ 37,170 + Mat: ₹ 18,591", 
+            color: "#962524",            
             payoutSchedule: { 11: 18591, 12: 18591, 13: 18591, 14: 18591, 15: 49576 } 
         },
         { 
@@ -43,8 +39,7 @@ export const POLICY_DATA = {
             id: "79079094",
             uin: "107N105V02",
             clientId: "61491746",
-            color: "#e63946",
-            maturityAmt: "₹ 3,23,695 + Bonus"
+            color: "#e63946"
         },
         { 
             logo: "image_5d7f80.png",
@@ -54,51 +49,45 @@ export const POLICY_DATA = {
             color: "#e63946",
             bonusAmount: 22050,
             bonusStartYear: 2,
-            income: "Est. Yearly: ₹ 23,140", maturityAmt: "₹ 7,11,994 + Bonus" 
+            income: "Est. Yearly: ₹ 23,140"            
         },
         { 
             logo: "image_86c684.png",
             id: "503-9562771",
             uin: "130A012V02",
-            color: "#005a9c",
-            maturityAmt: "Unit Value"
+            color: "#005a9c"
         },
         { 
             logo: "image_7a80d9.png",
             id: "608480927",
             uin: "104L082V02",
-            color: "#ae125d",
-            maturityAmt: "Unit Value"            
+            color: "#ae125d"
         },
         { 
             logo: "image_349b41.png",
             id: "425335400",
             uin: "116L153V02",
-            color: "#005a9c",
-            maturityAmt: "Unit Value"            
+            color: "#005a9c"
         },
         { 
             logo: "image_349b41.png",
             id: "378816898",
             uin: "116L181V01",
-            color: "#005a9c",
-            maturityAmt: "Unit Value"
+            color: "#005a9c"
         },
         { 
             logo: "image_d82175.png",
             id: "K7639543",
             uin: "N/A",
             clientId: "N/A",
-            color: "#9b2226",
-            maturityAmt: "Unit Value"
+            color: "#9b2226"
         },
         { 
             logo: "image_cda45c.png",
             id: "3N307267702",
             uin: "N/A",
             clientId: "N/A",
-            color: "#00B4EE",
-            maturityAmt: "Unit Value"
+            color: "#00B4EE"
         }
     ],
     singapore: [
