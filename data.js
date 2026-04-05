804fd8615cd0cbc129c96aa353f9b5ba4c29d8c3
sananpah <127280855+sananpah@users.noreply.github.com>
2026-04-06 04:02:02 +0800

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,6 @@
 export const POLICY_DATA = {
     india: [
         { 
-            logo: "image_4e0b3d.png",
             id: "54751949",
             uin: "128N171V01",
             clientId: "60938507",
@@ -10,7 +9,6 @@ export const POLICY_DATA = {
             payoutSchedule: { 11: 133974, 12: 133974, 13: 133974, 14: 133974, 15: 133974, 16: 160769, 17: 160769, 18: 160769, 19: 160769, 20: 160769, 21: 187564, 22: 187564, 23: 187564, 24: 187564, 25: 187564, 26: 214359, 27: 214359, 28: 214359, 29: 214359, 30: 214359 }
         },
         { 
-            logo: "image_4e0b3d.png",
             id: "54679062",
             uin: "128N161V04",
             clientId: "60938507",
@@ -19,7 +17,6 @@ export const POLICY_DATA = {
             defermentYear: 2031
         },
         { 
-            logo: "image_4e0b3d.png",
             id: "54040166",
             uin: "128N154V02",
             clientId: "60938507",
@@ -27,7 +24,6 @@ export const POLICY_DATA = {
             payoutSchedule: { 8: 100000, 9: 100000, 10: 100000, 11: 100000, 12: 100000, 13: 100000, 14: 100000, 15: 100000, 16: 100000, 17: 100000, 18: 100000, 19: 100000, 20: 100000, 21: 100000, 22: 100000, 23: 100000, 24: 100000, 25: 100000, 26: 100000, 27: 100000, 28: 100000 }
         },
         { 
-            logo: "image_4e0b3d.png",
             id: "53044042",
             uin: "128N053V02",
             clientId: "60938507",
@@ -35,14 +31,12 @@ export const POLICY_DATA = {
             payoutSchedule: { 11: 18591, 12: 18591, 13: 18591, 14: 18591, 15: 49576 } 
         },
         { 
-            logo: "image_5d7f80.png",
             id: "79079094",
             uin: "107N105V02",
             clientId: "61491746",
             color: "#e63946"
         },
         { 
-            logo: "image_5d7f80.png",
             id: "79425536",
             uin: "107N124V01",
             clientId: "61491746",
@@ -52,38 +46,32 @@ export const POLICY_DATA = {
             income: "Est. Yearly: ₹ 23,140"            
         },
         { 
-            logo: "image_86c684.png",
             id: "503-9562771",
             uin: "130A012V02",
             color: "#005a9c"
         },
         { 
-            logo: "image_7a80d9.png",
             id: "608480927",
             uin: "104L082V02",
             color: "#ae125d"
         },
         { 
-            logo: "image_349b41.png",
             id: "425335400",
             uin: "116L153V02",
             color: "#005a9c"
         },
         { 
-            logo: "image_349b41.png",
             id: "378816898",
             uin: "116L181V01",
             color: "#005a9c"
         },
         { 
-            logo: "image_d82175.png",
             id: "K7639543",
             uin: "N/A",
             clientId: "N/A",
             color: "#9b2226"
         },
         { 
-            logo: "image_cda45c.png",
             id: "3N307267702",
             uin: "N/A",
             clientId: "N/A",
@@ -92,39 +80,33 @@ export const POLICY_DATA = {
     ],
     singapore: [
          { 
-            logo: "image_7aee9a.png", 
             id: "34874419", 
             color: "#ED1B2E",                         
             withdrawals: [9195], 
             surrenderCharges: { 1: 0 }
         },
         { 
-            logo: "image_7aee9a.png", 
             id: "33401114", 
             color: "#ED1B2E",             
             withdrawals: [27481.38, 12072.37], 
             surrenderCharges: { 1: 0 }
         },
         { 
-            logo: "image_4b9b3f.png",
             id: "U126327913",
             color: "#d31145",
             surrenderCharges: { 1: 100, 2: 100, 3: 80, 4: 70, 5: 60, 6: 50, 7: 45, 8: 35, 9: 20, 10: 5, 11: 0 }
         },
         { 
-            logo: "image_d1e382.png",
             id: "2451556041",
             color: "#00a758",            
             surrenderCharges: { 1: 100, 2: 100, 3: 79, 4: 60, 5: 50, 6: 47, 7: 44, 8: 21, 9: 16, 10: 8, 11: 0 }
         },
         { 
-            logo: "image_8bcf5a.png",
             id: "104-1315514",
             color: "#db0011",            
             surrenderCharges: { 1: 100, 2: 52, 3: 49, 4: 45, 5: 40, 6: 32, 7: 26, 8: 20, 9: 14, 10: 8, 11: 0 }
         },
         { 
-            logo: "image_89093e.png",
             id: "P4172398", 
             color: "#E60000",            
             surrenderCharges: { 1: 100, 2: 100, 3: 80, 4: 60, 5: 50, 6: 45, 7: 40, 8: 20, 9: 15, 10: 10, 11: 0 }
