4dcd3115969b9202a0277b5ecb8e36f0bff5846e
sananpah <127280855+sananpah@users.noreply.github.com>
2026-04-11 01:43:54 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,46 +1,22 @@
 // data.js — Policy master list
 export const POLICY_DATA = {
     india: [
-        { 
-            id: "54751949", uin: "128N171V01", clientId: "60938507", color: "#962524"           
-        },
-        { 
-            id: "54679062", uin: "128N161V04", clientId: "60938507", color: "#962524"
-        },
-        { 
-            id: "54040166", uin: "128N154V02", clientId: "60938507", color: "#962524"            
-        },
-        { 
-            id: "53044042", uin: "128N053V02", clientId: "60938507", color: "#962524" 
-        },
-        { 
-            id: "79079094", uin: "107N105V02", clientId: "61491746", color: "#e63946" 
-        },
-        { 
-            id: "79425536", uin: "107N124V01", clientId: "61491746", color: "#e63946"
-        },
-        { 
-            id: "503-9562771", uin: "130A012V02", color: "#005a9c" 
-        },
-        { 
-            id: "608480927", uin: "104L082V02", color: "#ae125d" 
-        },
-        { 
-            id: "425335400", uin: "116L153V02", color: "#005a9c" 
-        },
-        { 
-            id: "378816898", uin: "116L181V01", color: "#005a9c" 
-        },
-        { 
-            id: "K7639543", uin: "N/A", clientId: "N/A", color: "#9b2226" 
-        },
-        { 
-            id: "3N307267702", uin: "N/A", clientId: "N/A", color: "#00B4EE" 
-        }
+        { id: "54751949", uin: "128N171V01", clientId: "60938507", color: "#962524"},
+        { id: "54679062", uin: "128N161V04", clientId: "60938507", color: "#962524"},
+        { id: "54040166", uin: "128N154V02", clientId: "60938507", color: "#962524"},
+        { id: "53044042", uin: "128N053V02", clientId: "60938507", color: "#962524"},
+        { id: "79079094", uin: "107N105V02", clientId: "61491746", color: "#e63946"},
+        { id: "79425536", uin: "107N124V01", clientId: "61491746", color: "#e63946"},
+        { id: "503-9562771", uin: "130A012V02", color: "#005a9c"},
+        { id: "608480927", uin: "104L082V02", color: "#ae125d"},
+        { id: "425335400", uin: "116L153V02", color: "#005a9c"},
+        { id: "378816898", uin: "116L181V01", color: "#005a9c"},
+        { id: "K7639543", uin: "N/A", clientId: "N/A", color: "#9b2226"},
+        { id: "3N307267702", uin: "N/A", clientId: "N/A", color: "#00B4EE"}
     ],
     singapore: [
-        { id: "34874419",   color: "#ED1B2E", withdrawals: [9195], surrenderCharges: { 1: 0 } },
-        { id: "33401114",   color: "#ED1B2E", withdrawals: [27481.38, 12072.37], surrenderCharges: { 1: 0 } },
+        { id: "34874419",   color: "#ED1B2E", surrenderCharges: { 1: 0 } },
+        { id: "33401114",   color: "#ED1B2E", surrenderCharges: { 1: 0 } },
         { id: "U126327913", color: "#d31145", surrenderCharges: { 1: 100, 2: 100, 3: 80, 4: 70, 5: 60, 6: 50, 7: 45, 8: 35, 9: 20, 10: 5, 11: 0 } },
         { id: "2451556041", color: "#00a758", surrenderCharges: { 1: 100, 2: 100, 3: 79, 4: 60, 5: 50, 6: 47, 7: 44, 8: 21, 9: 16, 10: 8, 11: 0 } },
         { id: "104-1315514",color: "#db0011", surrenderCharges: { 1: 100, 2: 52, 3: 49, 4: 45, 5: 40, 6: 32, 7: 26, 8: 20, 9: 14, 10: 8, 11: 0 } },
