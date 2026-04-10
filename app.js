17ff74b539b6cda9ebd9bacf10d744eebc519121
sananpah <127280855+sananpah@users.noreply.github.com>
2026-04-11 01:38:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /* app.js - v1.1.0 */
-import { syncWithGoogleSheets } from './loader.js?v=1.0.8';
-import { createPolicyCard } from './component_in.js?v=1.0.8';
-import { createSGCard } from './component_sg.js?v=1.0.8';
-import { createHealthCard } from './health.js?v=1.0.8';
-import { healthData } from './data_health.js?v=1.0.8';
-import { POLICY_DATA } from './data.js?v=1.0.8';
-import { toNum, raw, parseDate, safeParseDate, safeGetYear, monthMap, autoFmt, checkIsDueSoon, calculatePortfolioTotals, calculateFamilyBreakdown, calculateHealthTotals } from './utils.js?v=1.0.8';
+import { syncWithGoogleSheets } from './loader.js?v=1.0.9';
+import { createPolicyCard } from './component_in.js?v=1.0.9';
+import { createSGCard } from './component_sg.js?v=1.0.9';
+import { createHealthCard } from './health.js?v=1.0.9';
+import { healthData } from './data_health.js?v=1.0.9';
+import { POLICY_DATA } from './data.js?v=1.0.9';
+import { toNum, raw, parseDate, safeParseDate, safeGetYear, monthMap, autoFmt, checkIsDueSoon, calculatePortfolioTotals, calculateFamilyBreakdown, calculateHealthTotals } from './utils.js?v=1.0.9';
 
 window.currentCategory = 'india';
 let localPolicyData = POLICY_DATA;
