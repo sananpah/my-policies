1d8e7c681c1ff3df7272691175eb13135d042227
sananpah <127280855+sananpah@users.noreply.github.com>
2026-04-15 02:55:10 +0800

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,9 +1,7 @@
 /* loader.js - v4.5.25 - Minimal Fix: No Variable Changes */
-import { toNum, autoFmt, monthMap, getColorMap } from './utils.js?v=1.0.3';
+import { toNum, autoFmt, monthMap, getColorMap, githublogo } from './utils.js?v=1.0.3';
 
 const SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vThDQvcwmWKs2UwOfG57DQBOBnJX-9hsRKOQTUgALiM3uxs-VGzD2KN8JoWNAQltH6IkgAGhPTNFEvb/pub?gid=866869416&single=true&output=csv";
-const githubLogo = "https://raw.githubusercontent.com/sananpah/my-policies/main/assets/logo/";
-
 
 export async function syncWithGoogleSheets(masterList) {
     const TODAY = new Date();
